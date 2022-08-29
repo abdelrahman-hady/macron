942d2f254256673698420d12ede16358e9666413
Opeyemi John Ilesanmi <opeyemi.ilesanmi@scandiweb.com>
2022-08-29 14:35:49 +0100

    shipment custom table declaration and comments

--- app/code/Macron/Shipment/registration.php
+++ app/code/Macron/Shipment/registration.php
@@ -1,4 +1,12 @@
 <?php
+
+/**
+ * @category    Macron
+ * @package     Macron_Shipment
+ * @author      Opeyemi Ilesanmi <opeyemi.ilesanmi@scandiweb.com | info@scandiweb.com>
+ * @copyright   Copyright (c) 2022 Scandiweb, Inc (http://scandiweb.com)
+ */
+
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
