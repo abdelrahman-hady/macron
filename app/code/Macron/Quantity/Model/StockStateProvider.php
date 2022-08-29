7396c5a7a8fa229d06daca8844b6bef924f203da
Vladyslav Ivashchenko <vladyslav.ivashchenko@scandiweb.com>
2022-08-29 17:14:46 +0300

    MCRB-191 fixed code styles and added docs

--- app/code/Macron/Quantity/Model/StockStateProvider.php
+++ app/code/Macron/Quantity/Model/StockStateProvider.php
@@ -1,21 +1,29 @@
 <?php
 
+/**
+ * @category    Macron
+ * @package     Macron_Quantity
+ * @author      Vladyslav Ivashchenko <vladyslav.ivashchenko@scandiweb.com | info@scandiweb.com>
+ * @copyright   Copyright (c) 2022 Scandiweb, Inc (http://scandiweb.com)
+ */
+
+declare(strict_types=1);
+
 namespace Macron\Quantity\Model;
 
 use Magento\CatalogInventory\Api\Data\StockItemInterface;
+use Magento\CatalogInventory\Model\StockStateProvider as Source;
 
-class StockStateProvider extends \Magento\CatalogInventory\Model\StockStateProvider
+class StockStateProvider extends Source
 {
-
     /**
      * Check quantity
      *
      * @param StockItemInterface $stockItem
      * @param int|float $qty
-     * @exception \Magento\Framework\Exception\LocalizedException
      * @return bool
      */
-    public function checkQty(StockItemInterface $stockItem, $qty)
+    public function checkQty(StockItemInterface $stockItem, $qty): bool
     {
         return true;
     }
