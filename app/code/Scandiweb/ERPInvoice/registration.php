7d4af29a05829bfc44239550ea103bf36500fc49
abdelhakk-bakry <abdelhakk.bakry@scandiweb.com>
2022-08-30 23:23:29 +0100

    Applied docs to all files

--- app/code/Scandiweb/ERPInvoice/registration.php
+++ app/code/Scandiweb/ERPInvoice/registration.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @category Macron
+ * @author    Abdelhakk Bakry <abdelhakk.bakry@scandiweb.com | info@scandiweb.com>
+ * @license   http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0 (OSL-3.0)
+ * @copyright Copyright (c) 2022 Scandiweb, Inc (https://scandiweb.com)
+ */
 
 use Magento\Framework\Component\ComponentRegistrar;
 
@@ -7,3 +13,4 @@ ComponentRegistrar::register(
     'Scandiweb_ERPInvoice',
     __DIR__
 );
+
