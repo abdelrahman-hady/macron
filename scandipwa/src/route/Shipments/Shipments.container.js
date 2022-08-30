3b05128476f2a6847330157bb863cec4e5276262
Opeyemi John Ilesanmi <opeyemi.ilesanmi@scandiweb.com>
2022-08-30 23:08:53 +0100

    MCRB-209-Shipment page:Title,BreadCrumb and Status

---
+++ scandipwa/src/route/Shipments/Shipments.container.js
@@ -0,0 +1,80 @@
+/*
+ * @category  Macron
+ * @author    Opeyemi Ilesanmi <opeyemi.ilesanmi@scandiweb.com | info@scandiweb.com>
+ * @license   http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0 (OSL-3.0)
+ * @copyright  Copyright (c) 2022 Scandiweb, Inc (http://scandiweb.com) (c) 2022 Scandiweb, Inc (https://scandiweb.com)
+ */
+
+import PropTypes from 'prop-types';
+import { PureComponent } from 'react';
+import { connect } from 'react-redux';
+
+import { updateMeta } from 'Store/Meta/Meta.action';
+import { changeNavigationState } from 'Store/Navigation/Navigation.action';
+import { TOP_NAVIGATION_TYPE } from 'Store/Navigation/Navigation.reducer';
+
+import Shipments from './Shipments.component';
+
+export const BreadcrumbsDispatcher = import(
+    /* webpackMode: "lazy", webpackChunkName: "dispatchers" */
+    'Store/Breadcrumbs/Breadcrumbs.dispatcher'
+);
+
+/** @namespace Scandipwa/Route/Shipments/Container/mapStateToProps */
+export const mapStateToProps = (_state) => ({
+});
+
+/** @namespace Scandipwa/Route/Shipments/Container/mapDispatchToProps */
+export const mapDispatchToProps = (dispatch) => ({
+    updateMeta: (meta) => dispatch(updateMeta(meta)),
+    setHeaderState: (stateName) => dispatch(changeNavigationState(TOP_NAVIGATION_TYPE, stateName)),
+    updateBreadcrumbs: (breadcrumbs) => {
+        BreadcrumbsDispatcher.then(
+            ({ default: dispatcher }) => dispatcher.update(breadcrumbs, dispatch)
+        );
+    }
+});
+
+/** @namespace Scandipwa/Route/Shipments/Container */
+export class ShipmentsContainer extends PureComponent {
+    static propTypes = {
+        updateMeta: PropTypes.func.isRequired,
+        updateBreadcrumbs: PropTypes.func.isRequired
+    };
+
+    componentDidMount() {
+        this.updateMeta();
+        this.updateBreadcrumbs();
+    }
+
+    __construct(props) {
+        super.__construct(props, 'ShipmentsContainer');
+
+        this.updateBreadcrumbs();
+    }
+
+    updateMeta() {
+        const { updateMeta } = this.props;
+        updateMeta({ title: __('Shipments') });
+    }
+
+    updateBreadcrumbs() {
+        const { updateBreadcrumbs } = this.props;
+        const breadcrumbs = [
+            {
+                url: '/shipments',
+                name: __('Shipments')
+            }
+        ];
+
+        updateBreadcrumbs(breadcrumbs);
+    }
+
+    render() {
+        return (
+            <Shipments />
+        );
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShipmentsContainer);
