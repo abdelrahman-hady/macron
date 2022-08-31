fd038bcd6f28e81d43c2d359a4de93e3b49278b6
Deniss Dubinin <denissdubinin@gmail.com>
2022-08-31 12:18:49 +0300

    Create Macron_Erp module

---
+++ app/code/Macron/Erp/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @category    Macron
+ * @author      Deniss Dubinins <denissd@scandiweb.com | info@scandiweb.com>
+ * @copyright   Copyright (c) 2022 Scandiweb, Inc (http://scandiweb.com)
+ * @license     http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0 (OSL-3.0)
+ */
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Macron_Erp', __DIR__);
