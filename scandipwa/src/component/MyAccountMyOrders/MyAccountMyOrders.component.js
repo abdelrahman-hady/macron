adb761ce20050087ac565c74d792f0068df41052
MKom-Sama <mkomsany3@yahoo.com>
2022-08-31 14:07:59 +0200

    [MCRB-131] Get Available Statuses From OrderList.

--- scandipwa/src/component/MyAccountMyOrders/MyAccountMyOrders.component.js
+++ scandipwa/src/component/MyAccountMyOrders/MyAccountMyOrders.component.js
@@ -1,3 +1,5 @@
+import Field from 'Component/Field';
+import Loader from 'Component/Loader';
 import {
     MyAccountMyOrders as SourceMyAccountMyOrders
 } from 'SourceComponent/MyAccountMyOrders/MyAccountMyOrders.component';
@@ -6,7 +8,34 @@ import './MyAccountMyOrders.override.style';
 
 /** @namespace Scandipwa/Component/MyAccountMyOrders/Component */
 export class MyAccountMyOrdersComponent extends SourceMyAccountMyOrders {
-    // TODO implement logic
+    renderToolbar() {
+        const { sortOptions: { orderStatus }, updateOptions, statusOptions } = this.props;
+        return (
+            <div className="MyAccountMyOrders-Toolbar">
+            <Field
+              type="select"
+              label={ __('Sort by status') }
+              mix={ { block: 'MyAccountMyOrders', elem: 'SortByStatus' } }
+              options={ statusOptions }
+              value={ orderStatus }
+              events={ { onChange: (val) => updateOptions({ orderStatus: val }) } }
+            />
+            </div>
+        );
+    }
+
+    render() {
+        const { isLoading } = this.props;
+
+        return (
+            <div block="MyAccountMyOrders">
+                <Loader isLoading={ isLoading } />
+                { this.renderToolbar() }
+                { this.renderTable() }
+                { this.renderPagination() }
+            </div>
+        );
+    }
 }
 
 export default MyAccountMyOrdersComponent;
--- scandipwa/src/component/MyAccountMyOrders/MyAccountMyOrders.container.js
+++ scandipwa/src/component/MyAccountMyOrders/MyAccountMyOrders.container.js
@@ -6,11 +6,12 @@ import {
     mapStateToProps as sourceMapStateToProps,
     MyAccountMyOrdersContainer as SourceMyAccountMyOrdersContainer
 } from 'SourceComponent/MyAccountMyOrders/MyAccountMyOrders.container';
+import { scrollToTop } from 'Util/Browser';
 
+import MyAccountMyOrders from './MyAccountMyOrders.component';
 /** @namespace Scandipwa/Component/MyAccountMyOrders/Container/mapStateToProps */
 export const mapStateToProps = (state) => ({
     ...sourceMapStateToProps(state)
-    // TODO extend mapStateToProps
 });
 
 /** @namespace Scandipwa/Component/MyAccountMyOrders/Container/mapDispatchToProps */
@@ -21,7 +22,89 @@ export const mapDispatchToProps = (dispatch) => ({
 
 /** @namespace Scandipwa/Component/MyAccountMyOrders/Container */
 export class MyAccountMyOrdersContainer extends SourceMyAccountMyOrdersContainer {
-    // TODO implement logic
+    state = {
+        ...this.state,
+        sortOptions: {
+            orderStatus: 0 // Filters orders list by status
+        },
+        statusOptions: []
+    };
+
+    containerFunctions = {
+        ...this.containerFunctions,
+        updateOptions: this.updateOptions.bind(this)
+    };
+
+    componentDidMount() {
+        const { getOrderList } = this.props;
+        getOrderList(this._getPageFromUrl());
+        this.setState({ statusOptions: this._getStatusOptions() });
+    }
+
+    componentDidUpdate(prevProps) {
+        const { getOrderList } = this.props;
+        // eslint-disable-next-line no-unused-vars
+        const { sortOptions: { orderStatus } } = this.state;
+        const { location: prevLocation } = prevProps;
+
+        const prevPage = this._getPageFromUrl(prevLocation);
+        const currentPage = this._getPageFromUrl();
+
+        if (currentPage !== prevPage) {
+            getOrderList(this._getPageFromUrl());
+            scrollToTop();
+        }
+    }
+
+    _getStatusOptions() {
+        // Get Available Order Statuses Here
+        const { orderList: { items = [] } } = this.props;
+        const uniqueList = {};
+        if (items) {
+            // list available status
+            items.forEach((order) => {
+                // add to a hash map to avoid duplicates
+                uniqueList[order.status] = 1;
+            });
+
+            // correctly format statusList so it can be passed to Field
+            const statusArr = Array.from(Object.keys(uniqueList));
+            const statusOptions = [];
+            for (let i = 0; i < statusArr.length; ++i) {
+                statusOptions.push({
+                    id: i + 1,
+                    label: __(statusArr[i]),
+                    value: i + 1
+                });
+            }
+
+            return statusOptions;
+        }
+
+        return [];
+    }
+
+    containerProps() {
+        const { sortOptions, statusOptions } = this.state;
+        return {
+            ...super.containerProps(),
+            sortOptions,
+            statusOptions
+        };
+    }
+
+    updateOptions(option) {
+        this.setState(({ sortOptions }) => ({ sortOptions: { ...sortOptions, ...option } }));
+    }
+
+    render() {
+        return (
+            <MyAccountMyOrders
+              { ...this.containerProps() }
+              { ...this.containerFunctions }
+            />
+        );
+    }
 }
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(MyAccountMyOrdersContainer));
