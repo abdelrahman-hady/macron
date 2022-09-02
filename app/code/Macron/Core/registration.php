09f42b8cc436849f6e4dcf8d34161092f20d79f1
Deniss Dubinin <denissdubinin@gmail.com>
2022-09-02 12:09:28 +0300

    MCRB-208 - Add Xperpage configuration

--- app/code/Macron/Core/registration.php
+++ app/code/Macron/Core/registration.php
@@ -9,4 +9,4 @@ declare(strict_types=1);
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Macron_XperPage', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Macron_Core', __DIR__);
