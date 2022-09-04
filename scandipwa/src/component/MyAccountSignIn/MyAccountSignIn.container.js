1f7a38b9fc3908b28ff339f1603129f0093d713f
Lena Sinichenkova <lena.sinichenkova@scandiweb.com>
2022-09-04 23:48:45 +0300

    MCRB-181 Comments corrections

--- scandipwa/src/component/MyAccountSignIn/MyAccountSignIn.container.js
+++ scandipwa/src/component/MyAccountSignIn/MyAccountSignIn.container.js
@@ -1,6 +1,6 @@
 /*
  * @category Macron
- * @author    Omar Elshopky <omar.elshopky@scandiweb.com | info@scandiweb.com>
+ * @author    Lena Sinichenkova <lena.sinichenkova@scandiweb.com | info@scandiweb.com>
  * @license   http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0 (OSL-3.0)
  * @copyright Copyright (c) 2022 Scandiweb, Inc (https://scandiweb.com)
  */
