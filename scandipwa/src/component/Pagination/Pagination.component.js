8a8b68243e87b3203953c7c6ed1911470fdbc67a
shehab20089 <shehabmohsen1995@gmail.com>
2022-09-06 04:16:44 +0200

    minor fix

--- scandipwa/src/component/Pagination/Pagination.component.js
+++ scandipwa/src/component/Pagination/Pagination.component.js
@@ -1,3 +1,10 @@
+/*
+ * @category Macron
+ * @author    Shehab Mohsen <shehab.mohsen@scandiweb.com | info@scandiweb.com>
+ * @license   http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0 (OSL-3.0)
+ * @copyright Copyright (c) 2022 Scandiweb, Inc (https://scandiweb.com)
+ */
+
 import { Pagination as SourcePagination } from 'SourceComponent/Pagination/Pagination.component';
 
 import './Pagination.override.style.scss';
