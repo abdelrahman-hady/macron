f0f74307698ccf7678255e67a6fb3e5815d861bf
Vladyslav Ivashchenko <vladyslav.ivashchenko@scandiweb.com>
2022-09-12 13:57:35 +0300

    MCRB-199 - Fix MyAccountMyOrders undefined var

--- scandipwa/src/component/MyAccountMyOrders/MyAccountMyOrders.container.js
+++ scandipwa/src/component/MyAccountMyOrders/MyAccountMyOrders.container.js
@@ -110,7 +110,13 @@ export class MyAccountMyOrdersContainer extends SourceMyAccountMyOrdersContainer
     }
 
     componentDidUpdate(prevProps, prevState) {
-        const { getOrderList, ordersPerPageList, orderList: { pageInfo: { total_pages = 0 } } } = this.props;
+        const {
+            getOrderList, ordersPerPageList, orderList: {
+                pageInfo: {
+                    total_pages = 0
+                } = {}
+            }
+        } = this.props;
         const { sortOptions: { orderStatus }, ordersPerPage } = this.state;
         const { sortOptions: { orderStatus: prevOrderStatus }, ordersPerPage: prevOrdersPerPage } = prevState;
         const { location: prevLocation } = prevProps;
