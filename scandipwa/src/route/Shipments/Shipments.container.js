632d56f98a469a3e921af00ce99482e4bc45a18d
Opeyemi John Ilesanmi <opeyemi.ilesanmi@scandiweb.com>
2022-09-14 10:23:42 +0100

    add transformed list

--- scandipwa/src/route/Shipments/Shipments.container.js
+++ scandipwa/src/route/Shipments/Shipments.container.js
@@ -18,6 +18,7 @@ import { LocationType } from 'Type/Router.type';
 import { isSignedIn } from 'Util/Auth';
 import { scrollToTop } from 'Util/Browser';
 import BrowserDatabase from 'Util/BrowserDatabase';
+import { transformListViewAllowedValues } from 'Util/Config';
 import { fetchQuery, getErrorMessage } from 'Util/Request';
 import { appendWithStoreCode, getQueryParam } from 'Util/Url';
 
@@ -31,7 +32,7 @@ export const BreadcrumbsDispatcher = import(
 
 /** @namespace Scandipwa/Route/Shipments/Container/mapStateToProps */
 export const mapStateToProps = (state) => ({
-    shipmentsPerPageList: state.ConfigReducer.xperpage
+    shipmentsPerPageList: transformListViewAllowedValues(state.ConfigReducer.xperpage)
 });
 
 /** @namespace Scandipwa/Route/Shipments/Container/mapDispatchToProps */
