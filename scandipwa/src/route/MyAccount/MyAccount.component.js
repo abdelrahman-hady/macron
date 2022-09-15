77f208bcdb8e098638dec2a81b995d0c30d7967a
Saad Amir <59224810+saadamirpk@users.noreply.github.com>
2022-09-15 22:17:22 +0500

    MCRB-47 Only visible on Dashboard update

--- scandipwa/src/route/MyAccount/MyAccount.component.js
+++ scandipwa/src/route/MyAccount/MyAccount.component.js
@@ -73,7 +73,15 @@ export class MyAccountComponent extends SourceMyAccount {
     };
 
     renderInvoicesTable() {
-        return <DashboardInvoicesTable />;
+        const {
+            activeTab
+        } = this.props;
+
+        if (activeTab === 'DASHBOARD') {
+            return <DashboardInvoicesTable />;
+        }
+
+        return '';
     }
 
     renderContent() {
